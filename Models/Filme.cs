06d19da018c84c079cba00c496a6890baa5515c9
giovannipajeu1 <giovannipajeu@gmail.com>
2024-02-25 23:42:26 -0300

    Model Filme

--- Models/Filme.cs
+++ Models/Filme.cs
@@ -1,13 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FilmesApi.Models;
 
 
 public class Filme 
 {
-    public string Id { get; set; }
 
+    [Required(ErrorMessage = "Nome é Obrigatório")]
+    [MaxLength(50, ErrorMessage = "Nome do Filme Invalido")]
     public string Tittle { get; set; }
-
+    
+    [Required(ErrorMessage ="Genero é Obrigatorio")]
+    [MaxLength(20, ErrorMessage ="Genero Invalido")]
     public string Gender { get; set; }
 
+    [Required]
+    [Range(50,240, ErrorMessage ="Duração Invalida")]
     public int Duration { get; set; }
 }
