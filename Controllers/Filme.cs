1401538233770ec871b961f2396539a82479e10a
giovannipajeu1 <giovannipajeu@gmail.com>
2024-02-25 22:55:14 -0300

    Primeiro Commit

---
+++ Controllers/Filme.cs
@@ -0,0 +1,22 @@
+using FilmesApi.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+ [ApiController]
+ [Route("[controller]")]
+
+ public class FilmeController : ControllerBase
+ {
+
+    private static List<Filme> filmes = new List<Filme>();
+
+    public void AdicionaFilme(Filme filme)
+    {
+        filmes.Add(filme);
+    }
+
+ }
+
+
---
+++ Models/Filme.cs
@@ -0,0 +1,13 @@
+namespace FilmesApi.Models;
+
+
+public class Filme 
+{
+    public string Id { get; set; }
+
+    public string Tittle { get; set; }
+
+    public string Gender { get; set; }
+
+    public int Duration { get; set; }
+}
